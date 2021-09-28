d1eb1140fec57d27398673283464d828220749ad
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-09-28 12:30:35 +0200

    completing hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import styled from "styled-components";
-import tw from "styled-components";
+import tw from "tailwind-styled-components";
 
-import HeroImg from "./assets/images/hero.png";
+const Hero = tw.div`
+flex justify-around mx-auto md:flex-row items-center bg-hero-image-desktop
+bg-center bg-no-repeat
+`;
+
+const HeroText = tw.div`
+container py-40
+`;
+
+const HeroCol = tw.div`
+flex flex-col w-3/7 items-start`;
+
+type TextOverlineProps = {
+  $alt?: boolean;
+};
+const TextOverline = tw.div<TextOverlineProps>`
+${(p) => (p.$alt ? `text-white` : `text-primary`)}
+text-sm tracking-overline overline uppercase`;
+
+const TextDisplay = tw.h1`my-4 text-display text-white font-bold uppercase`;
+const TextHeroBody = tw.p`text-white text-heroBody font-extralight pr-8`;
+const Button = tw.button`bg-primary text-white uppercase text-button font-bold
+my-6 py-4 px-8  focus:outline-none focus:shadow-outline transform
+transition hover:scale-105 hover:bg-primaryLight duration-300 ease-in-out`;
 
 function App() {
   return (
-    <div className="bg-hero">
-      <div
-        id="hero"
-        className="flex container justify-around mx-auto md:flex-row items-center bg-black "
-      >
-        <div className="flex flex-col w-1/2 max-1/2">
-          <div className="w-4/6 flex flex-col justify-start items-start">
-            <p className="overline text-white">NEW PRODUCT</p>
-            <h1 className="my-4 text-5xl text-white font-bold leading-tight">
-              XX99 Mark II Headphones
-            </h1>
-            <p className="text-white">
-              Experience natural, lifelike audio and exceptional build quality
-              made for the passionate music enthusiast.
-            </p>
-            <button className=" bg-white text-gray-800 font-bold  my-6 py-4 px-8  focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-              Subscribe
-            </button>
-          </div>
-        </div>
-        <div className="w-1/2 max-1/2 overflow-y-hidden">
-          <img className="mx-auto w-full" src={HeroImg} alt="hero image" />
-        </div>
-      </div>
-    </div>
+    <Hero>
+      <HeroText>
+        <HeroCol>
+          <TextOverline $alt>New Product</TextOverline>
+          <TextDisplay>XX99 Mark II Headphones</TextDisplay>
+          <TextHeroBody>
+            Experience natural, lifelike audio and exceptional
+            <br />
+            build quality made for the passionate music
+            <br />
+            enthusiast.
+          </TextHeroBody>
+
+          <Button>Subscribe</Button>
+        </HeroCol>
+      </HeroText>
+    </Hero>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,82 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        "hero-image": "url('/src/assets/images/hero.png')",
+        "hero-image-desktop": "url('/src/assets/home/desktop/image-hero.jpg')",
+        "hero-image-tablet": "url('/src/assets/home/mobile/image-header.jpg')",
+        "hero-image-mobile": "url('/src/assets/home/tablet/image-header.jpg')",
+      },
+      width: {
+        "1/7": "14.2857143%",
+        "2/7": "28.5714286%",
+        "3/7": "42.8571429%",
+        "4/7": "57.1428571%",
+        "5/7": "71.4285714%",
+        "6/7": "85.7142857%",
       },
       colors: {
-        hero: "#0E0E0E",
+        primary: "#D87D4A",
+        primaryLight: "#fbaf85",
+        secondary: "#101010",
+        secondaryLight: "#F1F1F1",
+        secondaryLighter: "#FAFAFA",
+      },
+      fontFamily: {
+        manrope: ['"Manrope"', "sans-serif"],
+      },
+      letterSpacing: {
+        overline: "10px",
+      },
+      fontSize: {
+        display: [
+          "3.5rem",
+          {
+            letterSpacing: "0.125rem",
+            lineHeight: "3.625rem",
+          },
+        ],
+        body: [
+          "0.9375rem",
+          {
+            lineHeight: "1.5625rem",
+          },
+        ],
+        heroBody: [
+          "0.9375rem",
+          {
+            lineHeight: "1.5625rem",
+          },
+        ],
+        button: [
+          "0.8125rem",
+          {
+            letterSpacing: "0.06rem",
+          },
+        ],
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "600px",
+          },
+          "@screen md": {
+            maxWidth: "689px",
+          },
+          "@screen lg": {
+            maxWidth: "800px",
+          },
+          "@screen xl": {
+            maxWidth: "1110px",
+          },
+        },
+      });
+    },
+  ],
 };
